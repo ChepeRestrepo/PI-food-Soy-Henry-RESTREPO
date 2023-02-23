5e6f3f63b3890b6a77a52b0ba7ac57d2e6d4a502
Jose Restrepo Rueda <joserestrepo2901@gmail.com>
2023-02-23 17:00:50 -0500

    Se agrega carpeta routes, se agregan las rutas al index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,15 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
+const routeDiets = require("./route/routeDiets");
+const routeRecipes = require("./route/routeRecipe");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use("/recipes", routeRecipes);
+router.use("/diets", routeDiets);
 
 module.exports = router;
